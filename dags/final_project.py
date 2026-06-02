d81691c97fc927a1158af910da1b0c98c8171850
Christian Cadieux <cadieux_christian@hotmail.com>
2026-06-02 17:44:34 -0400

    Create StageToRedshiftOperator

--- dags/final_project.py
+++ dags/final_project.py
@@ -3,15 +3,17 @@ import pendulum
 import os
 from airflow.decorators import dag
 from airflow.operators.dummy import DummyOperator
-from operators import (StageToRedshiftOperator, LoadFactOperator,
-                       LoadDimensionOperator, DataQualityOperator)
+from operators.stage_redshift import StageToRedshiftOperator
+from operators.load_fact import LoadFactOperator
+from operators.load_dimension import LoadDimensionOperator
+from operators.data_quality import DataQualityOperator
 from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
     'start_date': pendulum.datetime(2018, 11, 1, tz='UTC'),
     "depends_on_past": False,
-    "retries": 3,
+    "retries": 0,
     "retry_delay": timedelta(minutes=5),
     "email_on_retry": False
 }
@@ -28,10 +30,21 @@ def final_project():
 
     stage_events_to_redshift = StageToRedshiftOperator(
         task_id='Stage_events',
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        table="staging_events",
+        s3_bucket="christian-cadieux",
+        s3_key="log-data",
+        format_as_json="s3://christian-cadieux/log_json_path.json"
     )
 
     stage_songs_to_redshift = StageToRedshiftOperator(
         task_id='Stage_songs',
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        table="staging_songs",
+        s3_bucket="christian-cadieux",
+        s3_key="song-data"
     )
 
     load_songplays_table = LoadFactOperator(
@@ -58,11 +71,14 @@ def final_project():
         task_id='Run_data_quality_checks',
     )
 
+    end_operator = DummyOperator(task_id='End_execution')
+
     start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table >> [
         load_user_dimension_table,
         load_song_dimension_table,
         load_artist_dimension_table,
         load_time_dimension_table
-    ] >> run_quality_checks
+    ] >> run_quality_checks >> end_operator
+    
 
 final_project_dag = final_project()
